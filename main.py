758373fc576ea412911cddbbd2ba3555700cb660
kaj <zekewotring@icloud.com>
2021-02-19 03:23:40 +0000

    CD command

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def vote(ctx):
     await ctx.send(embed=discord.Embed(description="[**Vote for the bot here**](https://top.gg/bot/811777444394172466/vote)", color = config.MAINCOLOR))
 
 # Cogs
-cogs = ['StatCord', 'Ls']
+cogs = ['StatCord', 'Ls', 'Cd']
 
 # Starts all cogs
 for cog in cogs:
