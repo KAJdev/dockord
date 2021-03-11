02a101226515c0eb2e4e7f2dc525200f1cc3e70e
Nytelife26 <xtylerjrx@gmail.com>
2021-03-11 00:12:08 +0000

    style(*): apply linting

--- Cogs/Passthrough.py
+++ Cogs/Passthrough.py
@@ -3,20 +3,21 @@ import os
 import Utils
 from Utils import Session
 
+
 class Passthrough(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.id == self.bot.user.id:
-            return
-        
-        if message.content.startswith("$"):
-            code, output = Session(message.author.id).send_command(message.content[1:], str(message.author.id))
-            await Utils.reply(message, output)
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.author.id == self.bot.user.id:
+			return
 
+		if message.content.startswith("$"):
+			code, output = Session(message.author.id).send_command(
+				message.content[1:], str(message.author.id))
+			await Utils.reply(message, output)
 
 
 def setup(bot):
-    bot.add_cog(Passthrough(bot))
+	bot.add_cog(Passthrough(bot))
--- Utils.py
+++ Utils.py
@@ -4,6 +4,7 @@ import config
 import pymongo
 import docker
 import os
+import logging
 import datetime
 
 mongo = pymongo.MongoClient(os.environ.get('DOCKORD_MONGO'))
@@ -12,74 +13,64 @@ users = db['users']
 
 docker_client = docker.from_env()
 
-async def out(ctx, output, title=None, img=None):
-    embed=discord.Embed(color=config.MAINCOLOR, description=f"```\n{output}```")
-    if title is not None:
-        if img is not None:
-            embed.set_author(name=title, icon_url=img)
-        else:
-            embed.set_author(name=title)
-    msg = await ctx.send(embed=embed)
-    return msg
 
-async def reply(ctx, output, title=None, img=None):
-    embed=discord.Embed(color=config.MAINCOLOR, description=f"```\n{output}```")
-    if title is not None:
-        if img is not None:
-            embed.set_author(name=title, icon_url=img)
-        else:
-            embed.set_author(name=title)
-    msg = await ctx.reply(embed=embed)
-    return msg
+def gen_embed(ctx, output, title=None, img=None):
+	embed = discord.Embed(
+		color=config.MAINCOLOR,
+		description=f"```\n{output}```"
+	)
+	if title is not None:
+		embed.set_author(name=title, icon_url=img)
+	return embed
+
 
 class Session():
+	def __init__(self, id=None):
+		super().__init__()
+		r = users.find_one({'id': id})
+		if r is None:
+			r = {
+				'id': id,
+				'container': None,
+				'last_command': datetime.datetime.utcnow()
+			}
+			users.insert_one(r)
+		for key, value in r.items():
+			setattr(self, key, value)
+		try:
+			self.container = docker_client.containers.get(self.container)
+			if self.container.status != "running":
+				self.container.start()
+		except Exception as e:
+			logging.exception(e)
+			self.container = None
 
-    def __init__(self, id=None):
-        super().__init__()
-        r = users.find_one({'id': id})
-        if r is None:
-            r = {
-                'id': id,
-                'container': None,
-                'last_command': datetime.datetime.utcnow()
-            }
-            users.insert_one(r)
-        for key, value in r.items():
-            setattr(self, key, value)
+	def refresh(self):
+		self.__init__(self.id)
 
-        try:
-            self.container = docker_client.containers.get(self.container)
-            if self.container.status != "running":
-                self.container.start()
-        except Exception as e:
-            self.container = None
+	def update(self, update, refresh=True):
+		r = users.update_one({'id': self.id}, update)
+		if refresh:
+			self.refresh()
+		return r
 
-    def refresh(self):
-        self.__init__(self.id)
+	def delete(self):
+		return users.delete_one({'id': self.id})
 
-    def update(self, update, refresh=True):
-        r = users.update_one({'id': self.id}, update)
-        if refresh:
-            self.refresh()
-        return r
+	def send_command(self, command, name):
+		if self.container is None:
+			self.create_container(name)
+		exit_code, output = self.container.exec_run(command)
+		return exit_code, output
 
-    def delete(self):
-        r = user.delete_one({'id': self.id})
-        return r
-
-    def send_command(self, command, name):
-        if self.container is None:
-            self.create_container(name)
-        exit_code, output = self.container.exec_run(command)
-        return exit_code, output
-    
-    def create_container(self, name):
-        if self.container is not None:
-            self.container.remove(force=True)
-        self.container = docker_client.containers.run('archlinux', detach=True, mem_limit="32m", name=name)
-        if self.container.status != "running":
-                self.container.start()
-        self.last_command = datetime.datetime.utcnow()
-        self.update({'$set': {'container': self.container.id, 'last_command': self.last_command}}, False)
-        return True
-
+	def create_container(self, name):
+		if self.container is not None:
+			self.container.remove(force=True)
+		self.container = docker_client.containers.run(
+			'archlinux', detach=True, mem_limit="32m", name=name)
+		if self.container.status != "running":
+			self.container.start()
+		self.last_command = datetime.datetime.utcnow()
+		self.update({'$set': {'container': self.container.id,
+                        'last_command': self.last_command}}, False)
+		return True
